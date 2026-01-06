4f4e0cd68b19202f1273f45f8592f8453ae35fa5
anhsmith <anhsmith@gmail.com>
2026-01-06 18:37:02 +1300

    updates description and readme

--- R/primertools-package.R
+++ R/primertools-package.R
@@ -1,7 +1,7 @@
 # R/primertools-package.R
-#' primertools: Utilities for PRIMER-ready CSVs
+#' primertools: Utilities for reading and writing PRIMER files
 #'
-#' Helper functions for writing PRIMER import files and related utilities.
+#' Helper functions for writing PRIMER import files and reading PRIMER export files.
 #'
 #' @docType package
 #' @name primertools
